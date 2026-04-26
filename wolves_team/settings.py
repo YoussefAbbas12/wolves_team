f41562aa10cf193a66d70e933b6dc24bc090e3da
YoussefAbbas12 <youssefabbas722@gmail.com>
2026-04-26 23:46:06 +0300

    Fix database and build settings

--- wolves_team/settings.py
+++ wolves_team/settings.py
@@ -122,6 +122,10 @@ import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
