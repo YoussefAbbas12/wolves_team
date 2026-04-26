75625d735988598c4c66064d99d1c0b2da08e684
YoussefAbbas12 <youssefabbas722@gmail.com>
2026-04-26 23:21:43 +0300

    ccc

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Department, TeamMember
+# Create your views here.
+
+def home(request):
+    leader = TeamMember.objects.filter(is_leader=True).first()
+    departments = Department.objects.all()
+
+    return render(request, 'home.html', {
+        'leader': leader,
+        'departments': departments
+    })
+
+def join(request):
+    return render(request, 'join.html')
